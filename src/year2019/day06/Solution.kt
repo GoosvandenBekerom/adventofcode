0c7172b0e0c8e1e1326db72f4c75492c81ba84af
gvandenbekerom <gvandenbekerom@bol.com>
2019-12-25 19:38:56 +0800

    Add day 6 part 2

--- src/year2019/day06/Solution.kt
+++ src/year2019/day06/Solution.kt
@@ -3,6 +3,7 @@ package year2019.day06
 import util.getFromResources
 
 fun main() {
+//    Part 1
     val inputAsNodes = "/2019/day06/input.txt".getFromResources()
         .split('\n')
         .map { it.split(')') }
@@ -15,8 +16,17 @@ fun main() {
     buildChain(com, inputAsNodes)
 
     val checksum = calculateChecksum(com)
-
     println(checksum)
+
+//    Part 2
+    val orbitalTransfers = "/2019/day06/input.txt".getFromResources()
+        .split('\n')
+        .map { it.split(')') }
+        .map { (parent, child) -> child to parent }
+        .toMap()
+        .calculateOrbitalTransfers("YOU", "SAN")
+
+    println(orbitalTransfers)
 }
 
 data class Node(val name: String, val children: MutableList<Node> = mutableListOf())
@@ -33,7 +43,6 @@ fun buildChain(node: Node, remainder: MutableList<Node>) : Node {
             buildChain(child, remainder)
         }
     }
-
     return node
 }
 
@@ -43,4 +52,22 @@ fun calculateChecksum(node: Node, depth: Int = 0) : Int {
         checksum += calculateChecksum(child, depth + 1)
     }
     return checksum + depth
+}
+
+fun calculatePath(planet: String, orbits: Map<String, String>) : List<String> =
+    if(planet == "COM")
+        listOf("COM")
+    else
+        calculatePath(orbits.getValue(planet), orbits) + planet
+
+fun Map<String, String>.calculateOrbitalTransfers(from: String, to: String) : Int {
+    val fromPath = calculatePath(from, this).toMutableList()
+    val toPath = calculatePath(to, this).toMutableList()
+
+    while(fromPath.first() == toPath.first()) {
+        fromPath.removeAt(0)
+        toPath.removeAt(0)
+    }
+
+    return fromPath.size + toPath.size - 2
 }
